eccb805bfec888c2a5da40826cdf624cef5cbba3
Taylor Smith <tgsmith61591@gmail.com>
2018-10-17 10:35:59 -0500

    CI: Get travis install working?

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -7,8 +7,8 @@
 set -e
 
 # Install pip requirements
-travis_wait travis_retry $PIP install numpy cython scikit-learn pybind11
-travis_wait travis retry $PIP install "implicit>=0.3.7" nmslib annoy pytest twine
+$PIP install numpy cython scikit-learn pybind11
+$PIP install "implicit>=0.3.7" nmslib annoy pytest twine
 
 # now run the python setup. This implicitly builds all the C code with build_ext
 travis_retry $PIP install -e .
